760e3ec9428fd642a011f741d529b2b32fa9e227
Andrew Hobden <andrew@hoverbear.org>
2014-11-05 09:59:27 -0800

    Add notes for Fieran

--- models/query.js
+++ models/query.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose'),
+    _ = require('lodash'),
+    Result = require('./result'),
     Schema = mongoose.Schema,
     ObjectId = Schema.Types.ObjectId,
     Mixed = Schema.Types.Mixed;
- 
+
 var schema = Schema({
     /**
      * Things that are required by the adapter.
@@ -19,6 +21,7 @@ var schema = Schema({
         type: String,
         required: true
     },
+/**** TODO: Fieran needs to change here because they are not ObjectIDs. ****/
     executions: {
         // The set of results for the query.
         type: [{
@@ -27,6 +30,7 @@ var schema = Schema({
         }],
         default: []
     },
+/***** TODO: End of things Fieran nees to change. ****/
     /**
      * Things that are interesting to the adapter, but it doesn't require them.
      */
@@ -53,6 +57,10 @@ var schema = Schema({
     }
     // TODO: Any additional info?
 });
+
+/**** TODO: Fieran needs to change to add the method. ****/
+
+/***** TODO: End of things Fieran nees to change. ****/
 
 // Define the model.
 module.exports = mongoose.model('Query', schema);

226e501d749dda15a3ae9945f2e9adf63ebc468f
Derek Roberts <derek.roberts@gmail.com>
2015-03-27 11:40:47 -0700

    Removed start.sh, using docker container

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit on errors and trace (print) exections
-#
-set -e
-
-npm install
-npm install request
-PORT=3003 MONGO_URI=mongodb://localhost:27019/query_composer_development npm start

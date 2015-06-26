8bf1739382d6d43715b5ea421e45b60bbc629d6f
Simon Diemert <simon.diemert@gmail.com>
2015-06-26 14:37:42 -0700

    Added retrospective demographics result management; added query type management in queries.js

---
+++ test/testQueries.js..js
@@ -0,0 +1,78 @@
+/**
+ * Create by sdiemert on 2015-06-26
+ *
+ * Unit tests for: lib/queries.js.
+ */
+
+var assert  = require('assert');
+var queries = require('../lib/queries.js');
+
+
+var testData = {
+
+    'PDC-001': queries.DEMOGRAPHICS,
+
+    'PDC-055': queries.MEDCLASS,
+
+    'PDC-053': queries.RATIO
+};
+
+describe("queries", function () {
+
+    beforeEach(function (done) {
+
+        queries.setQueries(testData);
+
+        done();
+
+    });
+
+    afterEach(function (done) {
+
+        done();
+
+    });
+
+    describe("#getQueryType()", function () {
+
+
+        beforeEach(function (done) {
+
+            done();
+
+        });
+
+        afterEach(function (done) {
+
+            done();
+
+        });
+
+        it("should return null if input is null", function (done) {
+
+            assert.equal(null, queries.getQueryType(null));
+
+            done();
+
+        });
+
+        it("should return null if query is not in data", function (done) {
+
+            assert.equal(null, queries.getQueryType('NOT_A_TITLE'));
+
+            done();
+
+        });
+
+        it("should return RATIO for PDC-053", function (done) {
+
+            assert.deepEqual(queries.getQueryType('PDC-053'), queries.RATIO);
+
+            done();
+
+        });
+
+
+    });
+
+});

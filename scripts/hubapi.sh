bac68a9bf5cf76deb8be270789efb65cdd8e0a35
Derek Roberts <derek.roberts@gmail.com>
2016-01-16 16:35:21 -0800

    Removed hubapi.sh, not in use

--- scripts/hubapi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /home/app/hubapi/
-#chpst -u app bash -c /home/app/hubapi/setup.sh #satisfy preconditions
-npm install # Install Dependencies into `.node_modules/`.
-npm start   # Start the application.
